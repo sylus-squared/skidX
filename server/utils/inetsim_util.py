4d3fda266098bfcb7e3facb70475fc2a3194bc19
SylusSquared7 <sylussquared@proton.me>
2024-05-26 11:51:19 +0100

    POC-Updated the inetsim code

--- server/utils/inetsim_util.py
+++ server/utils/inetsim_util.py
@@ -2,10 +2,10 @@ import subprocess
 import signal
 
 def run_inetsim(timeout):
-    process = subprocess.run("inetsim -whatever", shell=True, executable="/bin/bash")
+    process = subprocess.Popen("inetsim -whatever", shell=True, executable="/bin/bash")
 
     try:
-        client_process.wait(40)  # set timeout
+        process.wait(40)  # set timeout
     except subprocess.TimeoutExpired:
         process.send_signal(signal.SIGINT)
         print("terminated")
