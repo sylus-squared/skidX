bb989cc734847f81b543f5f66deeaa3d467d7621
SylusSquared7 <sylussquared@proton.me>
2024-05-27 21:26:31 +0100

    POC-Added the code to copy analysis reports to the reports folder

--- server/inetsim/run_inetsim.py
+++ server/inetsim/run_inetsim.py
@@ -1,6 +1,10 @@
 import subprocess
 import signal
 import os
+import shutil
+
+class AnalysisNotCompleteError(Exception):
+    pass # Thrown when the analysis direcory does not exist (Is this really necessary?)
 
 def run_inetsim(timeout, file_hash):
     if not os.path.exists(file_hash): # There should only be ONE analysis file per sample, this is just to make sure
@@ -15,9 +19,19 @@ def run_inetsim(timeout, file_hash):
         print("terminated")
 
 def get_report(file_hash):
-    if not os.path.exists("file_hash"):
+    if not os.path.exists(f"analysis/{file_hash}"):
         print("Analysis has not been completed yet")
-        # Throw some error
+        raise AnalysisNotCompleteError
 
-    
-    
+    file = os.listdir(f"analysis/{file_hash}")
+    source_dir = os.path.join(os.path.dirname(__file__), f"analysis/{file_hash}/{file[0]}")
+    dest_dir = os.path.join(os.path.dirname(__file__), '..', "webserver", "data/")
+
+    print("Source dir: " + source_dir)
+    print("Dest dir: " + dest_dir)
+
+    try:
+        shutil.copy(source_dir, dest_dir)
+    except FileNotFoundError:
+        print("File does not exist")
+    os.rename(dest_dir + file[0], dest_dir + file_hash + ".txt")
