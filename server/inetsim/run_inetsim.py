85a745b22b303872e46d6e4a9c14092e00eaeb10
SylusSquared7 <sylussquared@proton.me>
2024-05-27 09:50:23 +0100

    POC-Updated the inetsim code

--- server/inetsim/run_inetsim.py
+++ server/inetsim/run_inetsim.py
@@ -2,8 +2,11 @@ import subprocess
 import signal
 import os
 
-def run_inetsim(timeout):
-    process = subprocess.Popen("inetsim -whatever", shell=True, executable="/bin/bash")
+def run_inetsim(timeout, file_hash):
+    if not os.path.exists(file_hash): # There should only be ONE analysis file per sample, this is just to make sure
+        os.mkdir(file_hash)
+
+    process = subprocess.Popen(f"sudo inetsim --data data --conf inetsim.conf --report-dir {file_hash}", shell=True, executable="/bin/bash")
 
     try:
         process.wait(40)  # set timeout
@@ -11,5 +14,10 @@ def run_inetsim(timeout):
         process.send_signal(signal.SIGINT)
         print("terminated")
 
-def get_report():
-    pass
+def get_report(file_hash):
+    if not os.path.exists("file_hash"):
+        print("Analysis has not been completed yet")
+        # Throw some error
+
+    
+    
