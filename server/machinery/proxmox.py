3b75e995b9996ed1ffafce0a66063386bc13db80
SylusSquared7 <sylussquared@proton.me>
2024-06-01 17:34:49 +0100

    POC-Added the start of the proxmox machinery

--- server/machinery/proxmox.py
+++ server/machinery/proxmox.py
@@ -3,3 +3,9 @@ This is the machineary file for proxmox that allows the server to take snapshots
 an anlysis has been completed. Although this might not be necacery as most minecraft malware leaves no trace
 on the system, it is included as a precaution
 """
+import json
+
+with open("../webserver/config/config.json", "r") as read_file:
+    config = json.load(read_file)
+
+proxmox_URL = f"https://{config["machinery"]["proxmoxIP"]}:8006/api2/json/"
