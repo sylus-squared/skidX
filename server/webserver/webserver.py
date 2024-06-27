568d385e09dad1ef6790cfa95937a4443f4469c3
SylusSquared7 <sylussquared@proton.me>
2024-06-27 23:45:28 +0100

    POC-Included the requests library in setup files to allow for complete offline VM setup

--- server/webserver/webserver.py
+++ server/webserver/webserver.py
@@ -26,7 +26,7 @@ print("""
 
 app.config["UPLOAD_FOLDER"] = "uploads"
 ALLOWED_FILES = ["headlessmc-launcher-1.9.0.jar", ".minecraft", "HeadlessMC", "config", "background_blue.png",
- "background_red.png", "background_black.png", "background_purple.png", "background_white.png", "jre-8u411-windows-x64.exe", "python.exe"] # Files the endpoint is allowed to access from the /setup endpoint (to prevent LFI)
+ "background_red.png", "background_black.png", "background_purple.png", "background_white.png", "jre-8u411-windows-x64.exe", "python.exe", "requests"] # Files the endpoint is allowed to access from the /setup endpoint (to prevent LFI)
 analysis_in_progress = []
 
 # Setup propper logging
