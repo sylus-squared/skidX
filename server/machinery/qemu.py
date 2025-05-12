20557fd38b10562af569390ba79d17700b030480
sylus-squared <sylussquared@proton.me>
2025-05-12 23:13:41 +0100

    POC-Started adding the KVM machinery

---
+++ server/machinery/qemu.py
@@ -0,0 +1,22 @@
+"""
+Machinery for interacting with KVM VM's
+Its assumed the VM is on the same machine as the backend
+
+Every machinery file must have the following methods:
+initialize_machinery
+scan_file(file path)
+take_snapshot
+revert_snapshot
+"""
+
+def initialize_machinery():
+    pass
+
+def scan_file(file_path):
+    pass
+
+def take_snapshot():
+    pass
+
+def revert_snapshot():
+    pass
